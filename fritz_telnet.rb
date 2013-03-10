6e1f0c0f967c79e03bbeaefcbb4ffee4f3e40bb8
XORwell <christian.nennemann@gmail.com>
2013-03-10 08:27:53 +0100

    telnet initial commit

---
+++ fritz_telnet.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'bundler/setup'
+
+class FritzTelnet
+  #cat var/jason_boxinfo.xml
+  #cat var/config.def
+  #cat /var/flash/wlan.cfg
+  #cat /var/flash/ar7.cfg
+  #cat var/env
+  #cat var/tmp/wlan.log
+  def initialize
+
+
+  end
+end
